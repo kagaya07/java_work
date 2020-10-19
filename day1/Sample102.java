2a322f3ee4cbac34cd8f3da9ac9f3b2614d7b860
vagrant <vagrant@localhost.localdomain>
2020-10-19 15:08:41 +0000

    add day2..

---
+++ day1/Sample102.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Sample102 {
+	public static void main(String[] args) {
+		System.out.print(123);
+		System.out.println(456);
+
+		System.out.print("ABC");
+		System.out.println("DEF");
+		//日本語を表示
+		System.out.print("日本語");
+		System.out.println("の表示");
+	}
+
+}
