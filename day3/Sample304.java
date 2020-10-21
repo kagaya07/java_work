1cda962ba490d00f771fb24619d4be4ee592d0c4
vagrant <vagrant@localhost.localdomain>
2020-10-21 14:51:31 +0000

    add day3.4.5.6

---
+++ day3/Sample304.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class Sample304 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int dice = (int)(Math.random() * 7)+ 1;
+		System.out.println("ダイスの目:" + dice);
+		if (1 <= dice && dice <= 6) {
+			if (dice % 2 == 0) {
+				System.out.println("丁です。");
+			} else {
+				System.out.println("半です。");
+			}
+		} else {
+			System.out.println("範囲外の数値です。");
+		}
+
+
+	}
+
+}
---
+++ day4/Sample406.java
@@ -0,0 +1,20 @@
+package day4;
+
+public class Sample406 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("６がでたら終了");
+		//無限ループ
+		while(true) {
+			int dice = (int)(Math.random() * 6)+1;
+			System.out.println(dice);
+			if(dice == 6) {
+				break;//ループから抜ける
+			}
+		}
+		System.out.println("終了");
+
+	}
+
+}
